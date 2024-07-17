13f721a416c1b2662ce4885688a61dceb12af3bf
Lonesoldier230 <98001588+Lonesoldier230@users.noreply.github.com>
2024-07-18 00:04:15 +0545

    changes

--- sxj_quiz/round/models.py
+++ sxj_quiz/round/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Ques(models.Model):
+    question = models.CharField(max_length=100)
+    answer = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.question
+    
+class Mixed_Bag(models.Model):
+    subject = models.CharField(max_length=100, primary_key=True)
+    ques_ans = models.JSONField(default='dict', blank=True)
+    
+    def __str__(self) -> str:
+        return self.subject
